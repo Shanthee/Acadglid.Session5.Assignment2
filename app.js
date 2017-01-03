fbcfcece1800ad587298b93cdc2386f4219e804b
Shanthee Sampath <shantheesam@gmail.com>
2017-01-03 11:51:20 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// declaration of object
+var person = {
+	"name":"shanthee",
+	"age":29,
+	"dateofbirth":"17/Nov/1987",
+	"placeofbirth":"hosur"
+};
+
+
+//printing on console
+console.log("Name: " + person.name);
+console.log("Age: " + person.age);
+console.log("Date of Birth: " + person.dateofbirth);
+console.log("Place of Birth: " + person.placeofbirth);
